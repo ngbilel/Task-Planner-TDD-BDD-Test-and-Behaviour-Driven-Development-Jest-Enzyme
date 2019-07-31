80f59e232b09c0b2db77f0320c73a8d05d958c8e
Bilel <ng.bilel@gmail.com>
2019-07-31 20:23:34 +0200

    BDD: Using Describe, beforeEach, afterEach to avoid Redundancy, test polution ...

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,23 +3,38 @@ import {shallow } from 'enzyme';
 import App from './App';
 import { isTSAnyKeyword, isThisTypeAnnotation, isBigIntLiteral } from '@babel/types';
 
-const app = shallow(<App/>);
 
-//check that the component render correctly 
-it('render correctly', ()=>{
-    expect(app).toMatchSnapshot();
-})
+describe('App', ()=> {
 
-it('initializes the `state`  with  an empty list of tasks',()=>{
-    expect(app.state().tasks).toEqual([])
-})
+    const app = shallow(<App/>);
 
-it('add a new task to `state` when clicking the `add task` button', ()=>{
-    app.find('.btn-add').simulate('click');
-    expect(app.state().tasks).toEqual([{id:1}]);
-})
+    //check that the component render correctly 
+    it('render correctly', ()=>{
+        expect(app).toMatchSnapshot();
+    })
 
-it('add a new task to the render list when clicking the `add task` button',()=>{
-    app.find('.btn-add').simulate('click');
-    expect(app.find('.task-list').children().length).toEqual(2);
+    it('initializes the `state`  with  an empty list of tasks',()=>{
+        expect(app.state().tasks).toEqual([])
+    })
+
+    describe('When Clicking the `Add Task` Button',()=>{
+
+        beforeEach(()=>{
+            app.find('.btn-add').simulate('click');
+        })
+
+        afterEach(()=>{
+            app.setState({tasks:[]});
+        })
+
+        it('add a new task to `state` when clicking the `add task` button', ()=>{
+            expect(app.state().tasks).toEqual([{id:1}]);
+        })
+        
+        it('add a new task to the render list when clicking the `add task` button',()=>{
+            expect(app.find('.task-list').children().length).toEqual(1);
+        })
+
+    })
+
 })
